5a24034083233dd38407f4bec56aa8ea2cf0c1b1
NikIsakov <nikaarons@gmail.com>
2021-07-23 13:50:56 +0300

    Homework9 (1 и 2 зад)

---
+++ src/main/java/ru/geekbrains/happy/market/homework/repositories/ProductRepository.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.happy.market.homework.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import ru.geekbrains.happy.market.homework.model.Product;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    //List<Product> findAllByPriceBetween(int min, int max);
+
+    List<Product> findAllByCostGreaterThanEqualAndCostLessThan(int min, int max);
+
+    List<Product> findAllByCostIsLessThan(int max);
+
+    List<Product> findAllByCostGreaterThan(int min);
+
+    List<Product> findAllByTitleContains(String word);
+
+}
