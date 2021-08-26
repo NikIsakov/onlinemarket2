e454ae1fddfc33162a61e591d41aaa3344f06ea6
nik61088 <nik61088@gmail.com>
2021-08-26 07:06:17 +0300

    Homework_2_5 заглушка

--- src/main/java/ru/geekbrains/market/HappyMarketApplication.java
+++ src/main/java/ru/geekbrains/market/HappyMarketApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HappyMarketApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HappyMarketApplication.class, args);
 	}
